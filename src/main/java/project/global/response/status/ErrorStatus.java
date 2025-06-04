3446f10b698d8c56a6adf2151077db0f3abefc6a
zoo <anfdnfl@khu.ac.kr>
2025-06-04 13:49:38 +0900

    Merge

--- src/main/java/project/global/response/status/ErrorStatus.java
+++ src/main/java/project/global/response/status/ErrorStatus.java
@@ -15,13 +15,13 @@ public enum ErrorStatus {
 
     // 로그인,회원가입, 인증
     INVALID_TOKEN(HttpStatus.BAD_REQUEST, "JWT4000",
-        "유효하지 않은 토큰입니다."),
+            "유효하지 않은 토큰입니다."),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST, "JWT4001",
-        "해당 refresh token이 존재하지 않습니다."),
+            "해당 refresh token이 존재하지 않습니다."),
     TOKEN_IS_EXPIRED(HttpStatus.BAD_REQUEST, "JWT4002",
-        "만료된 토큰입니다."),
+            "만료된 토큰입니다."),
     AUTHENTICATION_TYPE_IS_NOT_BEARER(HttpStatus.BAD_REQUEST, "JWT4003",
-        "잘못된 토큰 타입입니다."),
+            "잘못된 토큰 타입입니다."),
 
     /*
      * mail
@@ -33,15 +33,15 @@ public enum ErrorStatus {
      * member
      */
     MEMBER_NOT_FOUND_BY_NICKNAME(HttpStatus.BAD_REQUEST, "MEMBER4000",
-        "해당 닉네임를 가진 회원이 존재하지 않습니다."),
+            "해당 닉네임를 가진 회원이 존재하지 않습니다."),
     MEMBER_NOT_FOUND_BY_EMAIL(HttpStatus.BAD_REQUEST, "MEMBER4001",
-        "해당 email을 가진 회원이 존재하지 않습니다."),
+            "해당 email을 가진 회원이 존재하지 않습니다."),
     MEMBER_DUPLICATE_BY_EMAIL(HttpStatus.BAD_REQUEST, "MEMBER4002",
-        "이미 가입된 email입니다. "),
+            "이미 가입된 email입니다. "),
     MEMBER_INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "MEMBER4003",
             "잘못된 비밀번호 입니다. "),
     MEMBER_DUPLICATE_BY_NICKNAME(HttpStatus.BAD_REQUEST, "MEMBER4004"
-        , "이미 있는 닉네임입니다."),
+            , "이미 있는 닉네임입니다."),
     MEMBER_NOT_FOUND_BY_ID(HttpStatus.BAD_REQUEST, "MEMBER4005",
             "해당 id를 가진 회원이 존재하지 않습니다."),
 
@@ -49,23 +49,16 @@ public enum ErrorStatus {
      * cart
      */
     CART_NOT_FOUND(HttpStatus.BAD_REQUEST, "CART4000",
-        "장바구니가 존재하지 않습니다."),
+            "장바구니가 존재하지 않습니다."),
     CART_ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "CART4001",
-        "장바구니에 해당 상품이 존재하지 않습니다."),
+            "장바구니에 해당 상품이 존재하지 않습니다."),
 
     /*
      * item
      */
     ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "ITEM4000",
-        "해당 상품이 존재하지 않습니다."),
-=======
-    MEMBER_INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "MEMBER4003",
-        "잘못된 비밀번호 입니다. "),
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER4004",
-    "해당 멤버가 존재하지 않습니다."),
->>>>>>> develop
+            "해당 상품이 존재하지 않습니다."),;
 
-    ;
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
