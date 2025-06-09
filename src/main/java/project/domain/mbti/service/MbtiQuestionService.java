13e7cf78d9dd9ad8ecd9fc975c0b688d652557d3
kseokc <104977065+kseokc@users.noreply.github.com>
2025-06-09 12:15:28 +0900

    fix : 피부 설문조사 다국어 기능 추가 (#27)
    1. 기존 질문API에 언어 Params 추가
    
    #Resolves : #21

--- src/main/java/project/domain/mbti/service/MbtiQuestionService.java
+++ src/main/java/project/domain/mbti/service/MbtiQuestionService.java
@@ -11,6 +11,7 @@ import project.domain.mbti.dto.MbtiQuestionConverter;
 import project.domain.mbti.dto.MbtiQuestionResponse;
 import project.domain.mbti.dto.MbtiQuestionResponse.QuestionInfoList;
 import project.domain.mbti.repository.MbtiQuestionRepository;
+import project.domain.member.enums.Language;
 import project.global.response.ApiResponse;
 import project.global.response.exception.GeneralException;
 import project.global.response.status.ErrorStatus;
@@ -22,9 +23,10 @@ public class MbtiQuestionService {
 
     private final MbtiQuestionRepository mbtiQuestionRepository;
 
-    public ApiResponse<QuestionInfoList> getQuestionInfoList(String axis) {
+    public ApiResponse<QuestionInfoList> getQuestionInfoList(String axis,String lang) {
 
-        List<MbtiQuestion> byAxis = mbtiQuestionRepository.findByAxisOrderByStartFirst(SkinAxis.valueOf(axis));
+        List<MbtiQuestion> byAxis = mbtiQuestionRepository.findByAxisOrderByStartFirst(SkinAxis.valueOf(axis),
+            Language.getLanguage(lang));
         QuestionInfoList questionInfoList = MbtiQuestionConverter.toQuestionInfoList(byAxis);
 
         return ApiResponse.onSuccess(questionInfoList);
