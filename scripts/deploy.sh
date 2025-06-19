282b9ab7e5de92d38244e3a7661fc96e21447a83
kseokc <104977065+kseokc@users.noreply.github.com>
2025-06-19 10:20:53 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,12 @@ ECR_REGISTRY=073658113926.dkr.ecr.ap-northeast-2.amazonaws.com
 
 cd $REPOSITORY
 
+# docker-compose.yml이 없으면 복사해서 이름 변경
+if [ ! -f docker-compose.yml ] && [ -f release-docker-compose.yml ]; then
+  echo "> 📝 Rename release-docker-compose.yml -> docker-compose.yml"
+  mv release-docker-compose.yml docker-compose.yml
+fi
+
 echo "> 🔵 PULL DOCKER IMAGE FROM ECR"
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin $ECR_REGISTRY
 
