cfcea7b383a97d8c2703d71f7d5352bb42e26c7e
kseokc <alvinseok@naver.com>
2025-07-01 16:00:48 +0900

    hotfix : 날짜 포맷 변경

--- src/main/java/project/domain/review/dto/ReviewResponse.java
+++ src/main/java/project/domain/review/dto/ReviewResponse.java
@@ -54,7 +54,7 @@ public abstract class ReviewResponse {
         private List<SelectOptionDTO> options;
         @JsonProperty("isRecommend")
         private boolean isRecommend;
-        @JsonFormat(pattern = "yyyy.MM.dd HH:mm:ss")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
         private LocalDateTime updatedAt;
     }
 
